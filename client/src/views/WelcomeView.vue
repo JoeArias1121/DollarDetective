8f4ec10aaddc20de942e98ba883d300ad9d43766
gianellarobles <gianellarobles@gmail.com>
2023-04-25 16:48:16 -0400

    new

--- client/src/views/WelcomeView.vue
+++ client/src/views/WelcomeView.vue
@@ -4,13 +4,20 @@
 <template>
   <main>
     <body>
+      <!-- Spacing between heading and navbar-->
+        <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </p>
+          <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </p>
+
     <h1 style="font-size: 8vw; text-align: center;"> Welcome to </h1>
   
   <h1 style="font-size: 8vw; text-align:center;"><i class="fa-solid fa-magnifying-glass-dollar has-text-black"></i><strong> Dollar Detective</strong></h1>
 
-  
-  <h2 style="font-size: 5vw; text-align: center; color: gray;"><strong>Status: Good</strong> </h2>
-    <h2 style="color:black; font-size: 4vw; text-align: center;"><strong>Expense of the day: 04/24/23</strong></h2> 
+  <!-- Spacing between heading and subheading-->
+  <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </p>
+   <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </p>
+
+  <h2 style="font-size: 4vw; text-align: center; color: gray;"><strong>Status: Good</strong> </h2>
+    <h2 style="color:black; font-size: 3vw; text-align: center;"><strong>Expense of the day: 04/27/23</strong></h2> 
 
      </body>
   </main>
