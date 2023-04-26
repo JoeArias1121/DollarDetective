b4d7879ab8b598f5b2c2035e54288c71199ba6ad
loiodicj1 <loiodicj1@newpaltz.edu>
2023-04-26 14:28:00 -0400

    added TestView (it's temporary)

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -12,27 +12,31 @@ import  router  from '../router'
             </div>
             <div class="box">
                 <nav class="level px-0 has-text-primary">
+                    <div class="hoverable right-border px-5" @click="router.push('/budget')">
+                        <i class="fa-solid fa-table-list has-text-black"></i>
+                        Budget
+                    </div>
 
-                <div class="hoverable right-border px-5" @click="router.push('/budget')">
-                    <i class="fa-solid fa-table-list has-text-black"></i>
-                    Budget
-                </div>
+                    <div class="hoverable right-border px-5">
+                        <i class="fa-solid fa-calendar-days has-text-black"></i>
+                        Calendar
+                    </div>
 
-                <div class="hoverable right-border px-5">
-                    <i class="fa-solid fa-calendar-days has-text-black"></i>
-                    Calendar
-                </div>
+                    <div class="hoverable right-border px-5">
+                        <i class="fa-solid fa-chart-line has-text-black"></i>
+                        Statistics
+                    </div>
 
-                <div class="hoverable right-border px-5">
-                    <i class="fa-solid fa-chart-line has-text-black"></i>
-                    Statistics
-                </div>
+                    <div class="hoverable right-border px-5" @click="router.push('/test')">
+                        <i class="fa-solid fa-frog has-text-black"></i>
+                        Test
+                    </div>
 
-                <div class="hoverable px-5">
-                    <i class="fa-solid fa-user-tie has-text-black"></i>
-                    User
-                </div>
-                    <div class="hoverable left-border px-5">
+                    <div class="hoverable right-border px-5">
+                        <i class="fa-solid fa-user-tie has-text-black"></i>
+                        User
+                    </div>
+                    <div class="hoverable px-5">
                         <i class="fa-solid fa-right-from-bracket has-text-black"></i>
                         Log Out
                     </div>
---
+++ client/src/views/TestView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+    import session from '@/stores/session';
+    import { updateUser } from '@/stores/users';
+
+    function noChange() {
+        if (session.user) {
+            updateUser(session.user).then(result => {
+                console.log(result)
+            })
+        }
+    }
+
+    function passChange() {
+        if (session.user) {
+            session.user.password = "pass"
+            updateUser(session.user).then(result => {
+                console.log(result)
+            })
+        }
+    }
+</script>
+
+<template>
+    <main>
+        <div class="box fades-in">
+            <div class="button is-bg" @click="passChange()">password change to 'pass'</div>
+        </div>
+    </main>
+</template>
