260f7abb780a80947080993960f659ea2202ee7c
loiodicj1 <loiodicj1@newpaltz.edu>
2023-04-26 14:28:21 -0400

    added TestView in router

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import SignInView from '../views/SignInView.vue'
 import BudgetView from '../views/BudgetView.vue'
 import WelcomeView from '../views/WelcomeView.vue'
+import TestView from '../views/TestView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/signIn',
       name: 'signIn',
       component: SignInView
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: TestView
     }
   ]
 })
