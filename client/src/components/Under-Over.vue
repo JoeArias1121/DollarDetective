f8302be13d11ad30f93d6383aea397b621819426
loiodicj1 <loiodicj1@newpaltz.edu>
2023-05-03 12:33:43 -0400

    Over-Under is confusing

--- client/src/components/Under-Over.vue
+++ client/src/components/Under-Over.vue
@@ -7,7 +7,8 @@ import session from '../stores/session';
 
 <template>
     <div>
-        <p><strong>Percantage of spending compared to goal: </strong>{{ underOverBudget().percentage }}%</p>
+        <p><strong>You've spent </strong>{{ underOverBudget().percentage }}% of your goal</p>
+        <p><strong>You've have </strong>${{ (session.user?.budgets.length != 0 ) ? session.user?.budgets[0].spendingLimit : 0 - underOverBudget().totalSpent }} left</p>
         <p><strong>Total spent this week:</strong> ${{ underOverBudget().totalSpent }}</p>
         <p><strong>Goal for the Week: </strong> ${{ (session.user?.budgets.length != 0 ) ? session.user?.budgets[0].spendingLimit : 0}}</p>
         <progress class="progress is-large" v-bind:value="underOverBudget().percentage" max="100">{{ underOverBudget().percentage }}</progress>
