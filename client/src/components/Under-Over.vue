274d78b3584e8508a504c33d5e6afa2eb105451b
JoeArias1121 <joearias1121@gmail.com>
2023-05-08 08:01:07 -0400

    Update Under-Over.vue

--- client/src/components/Under-Over.vue
+++ client/src/components/Under-Over.vue
@@ -10,7 +10,7 @@ import session from '../stores/session';
         <p><strong>You've spent </strong>{{ underOverBudget().percentage }}% of your goal</p>
         <p><strong>You've have </strong>${{ (session.user?.budgets.length != 0 ) ? session.user?.budgets[0].spendingLimit : 0 - underOverBudget().totalSpent }} left</p>
         <p><strong>Total spent this week:</strong> ${{ underOverBudget().totalSpent }}</p>
-        <p><strong>Goal for the Week: </strong> ${{ (session.user?.budgets.length != 0 ) ? session.user?.budgets[0].spendingLimit : 0}}</p>
+        <p><strong>Budget for the Week: </strong> ${{ (session.user?.budgets.length != 0 ) ? session.user?.budgets[0].spendingLimit : 0}}</p>
         <progress class="progress is-large" v-bind:value="underOverBudget().percentage" max="100">{{ underOverBudget().percentage }}</progress>
     </div>
 </template>
